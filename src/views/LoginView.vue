9939bb500c9ff33f3d24d868193b5ffeb6fbb99f
Chase J <46426781+blueberrycola@users.noreply.github.com>
2022-05-19 00:03:46 -0400

    added basic css

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="login-container">
-    <h1>Login:</h1>
+    
     <div class="login-form">
+      <h1>Login:</h1>
       <form action="myurl" method="GET" name="loginForm">
         <p><label for="username">Username:</label></p>
         <input type="text" name="username" id="uname">
@@ -10,22 +11,54 @@
         <input type="password" name="password" id="pword">
 
       </form>
-      <label class="switch">
-        Remember Me
-        <input type="checkbox">
-        <span class="slider"></span>
-      </label>
-      <button>Login</button>
+      <div class="login-buttonboard">
+        <label class="switch">
+          Remember Me
+          <input type="checkbox">
+          <span class="slider"></span>
+        </label>
+        <button>Login</button>
+      </div>
+
     </div>
   </div>
 </template>
 
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+input[type=text] {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  box-sizing: border-box;
+}
+input[type=password] {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  box-sizing: border-box;
+}
+.login-container {
+  position: center;
+  top: 150px;
+  left: 70px;
+  width: 1000px;
+  height: 550px;
+  box-shadow: rgba(255, 255, 255, 0.25) 0px 15px 15px, rgba(255, 255, 255, 0.12) 0px -12px 31px, rgba(255, 255, 255, 0.12) 0px 4px 6px, rgba(255, 255, 255, 0.17) 0px 12px 23px, rgba(255, 255, 255, 0.09) 0px -3px 15px;
+  border-radius: 25px;
+  background: rgb(0, 128, 79);
+}
+.login-form {
+  left: 50px;
+  top: 50px;
+  
+}
+.login-buttonboard{
+  padding: 5px;
+  right: 15px;
+  bottom: 5px;
+}
+.login-buttonboard .switch{
+  padding: 10px;
+  
 }
 </style>
